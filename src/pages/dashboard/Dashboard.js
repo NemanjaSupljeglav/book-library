9b87f23bee0f099921aae750496e8df0de91c9eb
N3manjaS <nemanjaaasssupljeglav@gmail.com>
2022-02-08 15:19:15 +0100

    deleted <

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -80,7 +80,7 @@ function Dashboard() {
             hideMenu={() => {
               hideNavMenu();
             }}
-          ></AllRoutes>
+          />
         </div>
       </BrowserRouter>
     </div>
