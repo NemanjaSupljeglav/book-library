7a77ab3941639786e9c5654ee2d198af3526a866
N3manjaS <nemanjaaasssupljeglav@gmail.com>
2022-02-09 08:33:44 +0100

    add redux quick start

--- src/pages/author/Author.js
+++ src/pages/author/Author.js
@@ -1,7 +1,30 @@
 import React from "react";
 import "./author.css";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../../redux/authorsSlice";
 function Author() {
-  return <div className="author-wrapper">Hello Author</div>;
+  const count = useSelector((state) => state.author.value);
+  const dispatch = useDispatch();
+  return (
+    <div className="author-wrapper">
+      <h1>Hello Author </h1>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Author;
--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -1,7 +1,30 @@
 import React from "react";
 import "./category.css";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "../../redux/categorySlice";
 function Category() {
-  return <div className="category-wrapper">Hello Category</div>;
+  const count = useSelector((state) => state.category.value);
+  const dispatch = useDispatch();
+  return (
+    <div className="category-wrapper">
+      <h1>Hello category </h1>
+      <div>
+        <button
+          aria-label="Increment value"
+          onClick={() => dispatch(increment())}
+        >
+          Increment
+        </button>
+        <span>{count}</span>
+        <button
+          aria-label="Decrement value"
+          onClick={() => dispatch(decrement())}
+        >
+          Decrement
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Category;
