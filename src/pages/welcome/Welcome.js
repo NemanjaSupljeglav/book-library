68b99d5676b390c3c1c7c4589602ab5b80de3a69
N3manjaS <nemanjaaasssupljeglav@gmail.com>
2022-02-09 11:57:14 +0100

    add background

--- src/pages/welcome/Welcome.js
+++ src/pages/welcome/Welcome.js
@@ -5,6 +5,16 @@ function Welcome() {
   return (
     <div className="welcome-wrapper">
       <h1 className="welcome-title">Welcome page</h1>
+      <p className="p-welcome">
+        Laboris excepteur est minim et consectetur elit ipsum. Ullamco minim
+        reprehenderit esse dolor. Nostrud commodo laborum non irure officia
+        deserunt nisi elit elit quis labore id voluptate ipsum. Proident laboris
+        sint proident labore sint nostrud mollit pariatur cillum ex amet.
+        Commodo ullamco in amet pariatur in commodo ex irure qui eiusmod Lorem
+        id. Aliquip officia dolore nostrud commodo sit ipsum ea elit aliquip
+        consectetur voluptate ad laboris aliquip. Exercitation dolore incididunt
+        laboris nulla elit non id ullamco incididunt ad.\r\n"
+      </p>
       <img
         className="welcome-background"
         src="https://images.unsplash.com/photo-1514593214839-ce1849100055?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
